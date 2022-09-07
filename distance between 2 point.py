861f2fca938f50f6da8ee871a2535da51ed41f7b
Tya09 <111039770+Tya09@users.noreply.github.com>
2022-09-07 20:16:56 +0700

    Add files via upload

---
+++ distance between 2 point.py
@@ -0,0 +1,20 @@
+#distance between 2 point
+# cara 1
+# x1 = float(input())
+# y1 = float(input())
+# x2 = float(input())
+# y2 = float(input())
+# p1 = x1, y1
+# p2 = x2, y2
+# print(p1)
+# print(p2)
+# import math
+# distance = math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
+# print(round(distance,4))
+
+#cara 2
+float(input()), float(input())
+float(input()), float(input())
+import math
+distance = math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
+print(round(distance,4))
