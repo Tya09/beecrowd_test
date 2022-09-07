93172d9a0ff19bf57e707cf6f531b848c0be6fe4
Tya09 <111039770+Tya09@users.noreply.github.com>
2022-09-07 20:31:34 +0700

    Add files via upload

---
+++ fuel spent.py
@@ -0,0 +1,12 @@
+# fuel spent
+#contoh 1 
+spent_car = 12 
+spent_time = int(input())
+average_speed = int(input())
+distance = average_speed * spent_time
+fuel = distance / spent_car
+print(round(fuel,3))
+
+
+#contoh 2 
+print(round(int(input()) * int(input()) / 12,3))
