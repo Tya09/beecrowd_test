68fa03178d43a0469cb2455bde0a12eefdea9a5f
Tya09 <111039770+Tya09@users.noreply.github.com>
2022-09-07 15:34:25 +0700

    Add files via upload

---
+++ Sphere.py
@@ -0,0 +1,12 @@
+#volume of sphere
+#cara 1
+pi = 3.14159
+R = int(input())
+
+volume = (4/3) * pi * (R**3)
+print("VOLUME =",round(volume,3))
+
+
+#cara 2 
+#meminimalisir variabel
+print("VOLUME =",round((4/3) * 3.14159 * (int(input())**3),3))
